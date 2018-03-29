9fb01350ec8c1491a9adb26ff1447f7cf00f8712
wangfw <wangfw@wangfwdeMac-mini.local>
2018-03-29 15:54:15 +0800

    init

---
+++ AAA/AAAView.h
@@ -0,0 +1,13 @@
+//
+//  AAAView.h
+//  LHHAlertView
+//
+//  Created by wangfw on 2018/3/29.
+//  Copyright © 2018年 澳视. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAView : UIView
+
+@end
---
+++ AAA/AAAView.m
@@ -0,0 +1,21 @@
+//
+//  AAAView.m
+//  LHHAlertView
+//
+//  Created by wangfw on 2018/3/29.
+//  Copyright © 2018年 澳视. All rights reserved.
+//
+
+#import "AAAView.h"
+
+@implementation AAAView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
